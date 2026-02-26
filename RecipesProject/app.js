bc4050259f008dbaf85cfe88aa03371f71822185
TOMLUCA7 <tomluca937@gmail.com>
2026-02-26 09:35:30 +0200

    feat: Initialize Express application with CORS, JSON parsing, static assets, and a custom request logger.

---
+++ RecipesProject/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { logger } from "./middlewares/logger.js";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!, this is recipe API");
+});
+
+app.all(/^(.*)$/, (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
