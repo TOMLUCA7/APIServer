b1329887113c35448863742f6bec814d406e27d0
TOMLUCA7 <tomluca937@gmail.com>
2026-03-04 11:10:20 +0200

    feat: implement user registration and login with bcrypt and JWT authentication.

---
+++ RecipesProject/controllers/authController.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10); // hash password
+    const userId = await User.create(username, email, hashedPassword); // create user
+    res.status(201).json({ message: "User created!", userId });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByEmail(email);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password); // compare password
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT_SECRET || "secret_key",
+      { expiresIn: "12h" },
+    ); // create token
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default {
+  register,
+  login,
+};
---
+++ RecipesProject/models/userModel.js
@@ -0,0 +1,20 @@
+import pool from "../db.js";
+
+const User = {
+  // create new user
+  create: async (username, email, hashedPassword) => {
+    const [result] = await pool.query(
+      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+      [username, email, hashedPassword],
+    );
+    return result.insertId;
+  },
+
+  // find user by email
+  findByEmail: async (email) => {
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+    return rows[0];
+  },
+};
