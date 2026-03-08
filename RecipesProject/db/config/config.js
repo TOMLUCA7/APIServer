64e03d11c62fcbbb6b2d6c6945ba4466e18aaaaf
TOMLUCA7 <tomluca937@gmail.com>
2026-03-08 13:36:14 +0200

    feat: Initialize Sequelize ORM by adding dependencies, configuration, and model setup files.

---
+++ RecipesProject/db/config/config.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
