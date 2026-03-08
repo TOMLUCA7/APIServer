4c069c1e598c0088bc9e02beaeded3949573260b
TOMLUCA7 <tomluca937@gmail.com>
2026-03-08 15:54:17 +0200

    feat: create users table migration and update database connection variable reference in config.

--- RecipesProject/db/config/config.js
+++ RecipesProject/db/config/config.js
@@ -3,15 +3,15 @@ dotenv.config();
 
 export default {
   development: {
-    use_env_variable: process.env.DB_CONNECTION,
+    use_env_variable: "DB_CONNECTION",
     dialect: "postgres",
   },
   test: {
-    use_env_variable: process.env.DB_CONNECTION,
+    use_env_variable: "DB_CONNECTION",
     dialect: "postgres",
   },
   production: {
-    use_env_variable: process.env.DB_CONNECTION,
+    use_env_variable: "DB_CONNECTION",
     dialect: "postgres",
   },
 };
