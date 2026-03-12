0475019dbdf7be6a4de49beeee2d2b6e08bcbbc9
TOMLUCA7 <tomluca937@gmail.com>
2026-03-12 19:36:18 +0200

    feat: add toaster component

---
+++ RecipesClient/src/components/ui/toaster.jsx
@@ -0,0 +1,26 @@
+import { ToastProvider, ToastViewport } from './toast'
+import { useToast } from '../../hooks/use-toast'
+
+import { Toast, ToastClose, ToastDescription, ToastTitle } from './toast'
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(({ id, title, description, action, ...props }) => (
+        <Toast key={id} {...props}>
+          <div className="grid gap-1">
+            {title ? <ToastTitle>{title}</ToastTitle> : null}
+            {description ? (
+              <ToastDescription>{description}</ToastDescription>
+            ) : null}
+          </div>
+          {action}
+          <ToastClose />
+        </Toast>
+      ))}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
