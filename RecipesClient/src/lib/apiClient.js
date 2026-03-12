3549429e6e97a8d138a1f8233b10684bf5f0dee9
TOMLUCA7 <tomluca937@gmail.com>
2026-03-12 20:20:46 +0200

    refactor: Migrate Tailwind CSS integration from PostCSS to the new `@tailwindcss/vite` plugin, removing `postcss.config.js` and `tailwind.config.js`.

--- RecipesClient/src/lib/apiClient.js
+++ RecipesClient/src/lib/apiClient.js
@@ -1,20 +1,20 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const TOKEN_STORAGE_KEY = 'auth_token'
+export const TOKEN_STORAGE_KEY = "auth_token";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
-})
+  baseURL: "http://localhost:3000",
+});
 
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem(TOKEN_STORAGE_KEY)
+    const token = localStorage.getItem(TOKEN_STORAGE_KEY);
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
-  (error) => Promise.reject(error)
-)
+  (error) => Promise.reject(error),
+);
 
-export default apiClient
+export default apiClient;
--- RecipesClient/src/pages/Register.jsx
+++ RecipesClient/src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { Link } from 'react-router-dom'
 import Layout from '../components/Layout'
 import { Button } from '../components/ui/button'
